e8a1b9cdae4f4fdc9bd72984f56165d87fe53bfa
Verb <nverbus@Dev-10.com>
2021-06-11 13:56:03 -0500

    initializing project

---
+++ Tournament_Verbus.py
@@ -0,0 +1,77 @@
+'''
+REQ
+
+Participants and their starting slots should be represented by an array.
+Do not put a value for empty in a slot.
+Participant names are a single string. Do not put first and last names in separate values.
+There will be at least 1 loop and at least 1 function.
+
+order of things
+
+
+functions
+
+    AddPlayer
+
+    RemovePlayer
+
+    ListofPlayers
+    
+    Individual player select?
+
+    NameSeparate?
+
+    NameSort?
+
+    NameTitle-er
+        
+        to regulate string entries
+
+
+
+initialize variables
+
+    tournament array list name thing
+
+    Separtated_Names_Alphabetical?
+
+
+THE START
+
+
+input
+    
+    How many players?
+
+
+
+loop- --while people are still being entered--
+
+#access ui though input
+
+input
+
+UI
+    Add player
+
+        specific slot
+
+    Remove player
+
+        both name and slot number required
+
+    List of players/player select?
+
+        ordered based on slots
+
+        additional inputs to sort alphabetically or by position?
+    
+    Finish
+
+        end while loop
+
+-loop
+    
+
+
+'''
